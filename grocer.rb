6a35d8b0c58f73ee196e82396f3327fbc2c5a908
Khalid Tissire <khalid.tissire@gmail.com>
2020-01-13 19:48:42 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,7 @@ $idx = -1
 
 def find_item_by_name_in_collection(name,collection)
   i = 0
-  while i < collection.length do
+  while i < collection.length
     if (collection[i][:item] === name.upcase)
       $idx = i
       return collection[i]
@@ -16,7 +16,7 @@ def consolidate_cart(cart)
   arr = []
   i = 0
   j = 0
-  while i < cart.length do
+  while i < cart.length
     if (find_item_by_name_in_collection(cart[i][:item], arr) === nil)
       arr << cart[i]
       arr[j][:count] = 1
@@ -32,9 +32,9 @@ end
 def apply_coupons(cart,coupons)
   arr = cart
   i = 0
-  while i < cart.length do
+  while i < cart.length
     j = 0
-    while j < coupons.length do
+    while j < coupons.length
       if (cart[i][:item] === coupons[j][:item] && cart[i][:count] >= coupons[j][:num])
         arr << {:item => "#{cart[i][:item]} W/COUPON", :price => coupons[j][:cost] / coupons[j][:num],:clearance => cart[i][:clearance],:count => (cart[i][:count] - (cart[i][:count] % coupons[j][:num]))}
           arr[i][:count] = cart[i][:count] % coupons[j][:num]
@@ -49,7 +49,7 @@ end
 def apply_clearance(cart)
   arr = []
   i = 0
-  while i < cart.length do
+  while i < cart.length
     cart[i][:clearance] ? price = (cart[i][:price] - cart[i][:price] * 0.2).round(2) : price = cart[i][:price]
     arr << {:item => cart[i][:item], :price => price,:clearance => cart[i][:clearance],:count => cart[i][:count]} 
     i += 1  
@@ -67,7 +67,7 @@ def checkout(cart,coupons)
   consolidated_cart = apply_clearance(consolidated_cart)
   sum = 0
   i = 0
-  while i < consolidated_cart.length do
+  while i < consolidated_cart.length
     sum += total_price_by_item(consolidated_cart[i])
     i += 1
   end  
